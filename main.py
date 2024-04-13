3d573ddc1edb22b001c985ad24765941384bdfac
František Brožka <sentientfanda@gmail.com>
2024-04-13 15:51:58 +0200

    Send only 5 characters to each call of utility 'toilet'. Wait a bit after each line of output.

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 # simple python3 script to ...
 import os
 import subprocess
+import time
 l=os.listdir('.')
 for f in l:
   if os.path.isfile(f):
     with open(f) as file:
       for line in file:
-        subprocess.run(['toilet','--filter','gay',line.rstrip()])
+        line=line.rstrip().replace(' ','').replace('\t','')
+        n=5
+        for i in range(0,len(line),n):
+          subprocess.run(['toilet','--filter','gay',line[i:i+n]])
+          time.sleep(0.27)

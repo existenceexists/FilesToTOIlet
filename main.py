bb3325a58f3c87426e2491e5ff452670e5cb04e5
František Brožka <sentientfanda@gmail.com>
2024-04-15 10:10:12 +0200

    modify description for argparse

--- main.py
+++ main.py
@@ -7,10 +7,7 @@ import time
 parser=argparse.ArgumentParser(
   prog='FilesLinesInDirToToilet',
   description="""
-Command line utility and simple wrapper around application and command line utility called TOIlet.
-This application will get each line of each file in present working directory (exluding subdirectories) 
-and send it to TOIlet to create colorful ascii art and output it to so-called stdout (i.e. standart output typically in type of applications called terminal emulators). 
-This simple small application is written in Python 3 so it is portable.
+Simple small CLI utility. This application gets each non-white-space character of each file in present working directory (exluding subdirectories) and create a colorful ascii art from it and output it to standart output (stdout). Each line of the ouput has the same width (number of characters (columns)). Width i.e. number of characters (columns) of each line may be set with the optional CLI argument '-w' (or '--width'). The CLI utility TOIlet is used and required to be installed so that it can be found in $PATH. This is a very simple wrapper around CLI utility TOIlet. Because this script is very small and simple it can be easily modified and played with. It is written in Python so it is portable. It works with Python 3 and probably with Python 2 also.
 """)
 parser.add_argument('-w','--width',type=int,default=shutil.get_terminal_size((80,20))[0],help="""Number of characters that will be sent to 'toilet'. Defaults to width i.e. number of columns (characters) of terminal emulator (as determined by the Python 3 method 'shutil.get_terminal_size'.""")
 args=parser.parse_args()
